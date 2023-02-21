0990bc5635b3e7aab2e9c6220bee5f92c8b9cca6
Gearboxpetrolhead <sandrinemalanga2001@gmail.com>
2023-02-21 11:57:25 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB")
 
 const app = express();
 
@@ -7,7 +9,22 @@ app.get("/", (req, res) => {
     res.send("<h2>Home Page</h2>");
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+startServer();
+
+
+
+
